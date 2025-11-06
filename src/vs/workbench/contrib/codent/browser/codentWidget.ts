8b758dcd8b87985e3fc99a382a97a79c6622feef
Xiaokuang Bai <baixiaokuang@gmail.com>
2025-11-06 18:16:52 +0800

    Refactor CodentWidget to properly initialize input and title elements in the render method

--- src/vs/workbench/contrib/codent/browser/codentWidget.ts
+++ src/vs/workbench/contrib/codent/browser/codentWidget.ts
@@ -16,20 +16,26 @@ export class CodentWidget extends Disposable implements ICodentWidget {
 	readonly domNode?: HTMLElement;
 	container!: HTMLElement;
 	listContainer!: HTMLElement;
+	input!: HTMLElement;
+	title!: HTMLElement;
 	constructor() {
 		super();
 	}
 
 	render(parent: HTMLElement) {
 		this.container = dom.append(parent, $('.interactive-session'));
-		dom.append(this.container, $('p', {}, 'Hello World!'));
+		this.title = dom.append(this.container, $('p.title', {}, 'Hello World!'));
 		this.listContainer = dom.append(this.container, $(`.interactive-list`));
 		this.createInput(this.container);
 		this.createList(this.listContainer);
 	}
 
 	private createInput(container: HTMLElement, options?: { renderFollowups: boolean; renderStyle?: 'compact' | 'minimal' }): void {
-
+		this.input = dom.append(container, $('input.interactive-session.chat-input-container'));
+		const button = dom.append(container, $('button', {}, 'Send'));
+		button.onclick = () => {
+			this.title.innerText = (this.input as HTMLInputElement).value;
+		};
 	}
 
 	private createList(listContainer: HTMLElement): void {
